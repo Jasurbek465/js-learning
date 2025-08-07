0250a698657dbf2fe1394db02d79f9202b838a3d
Jasurbek465 <abdurashidovjasurbek584@gmail.com>
2025-08-07 11:37:14 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// 1-misol
+let matn = "Bunga kirishdan ehtiyot bo'ling !";
+console.log(matn);
+
+// 2chi misol
+let name = "Sardor";
+let lastName = "Qobilov";
+let fullName = name + " " + lastName;
+let malumot = prompt("Ism va Sharifingiz?")
+alert("Ism Familya: " + fullName);
+
+// 3-misol
+let tugilganYil = prompt("Tug'ilgan yilingizni kiriting:");
+let hozirgiYil = new Date().getFullYear();
+let yosh = hozirgiYil - tugilganYil;
+
+alert("Siz " + yosh + " yoshdasiz.");
+
+// 5-misol
+let a = 12;
+let b = 34;
+let c = 46;
+let plus = a + b;
+console.log(plus === c);
+
+// 6-misol
+console.log(plus / 2);
+
+// 7-misol
+a = 12;
+b = 34;
+c = 45;
+
+let orta = (a + b + c) / 3;
+console.log(orta);
+
+
+
