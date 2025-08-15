6bf5e83974ff93b2d2dad00242c61a1dfd61c61c
Jasurbek465 <abdurashidovjasurbek584@gmail.com>
2025-08-15 15:10:37 +0500

    m

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// mashq 1.
+console.log("JavaScript oson til".substring(4, 10));
+
+// mashq 2. 
+console.log("olma,banan,gilos".split(","));
+
+// mashq 3.
+console.log("Bugun havo yaxshi".includes("yaxshi"));
+
+// mashq 4. 
+console.log("Men Python o‘rganaman".replace("Python", "JavaScript"));
+
+// mashq 5. 
+console.log("Salom".charAt(2));
+
+// mashq 6. 
+console.log("ABC".charCodeAt(0));
+
+// mashq 7. 
+console.log("JavaScript yaxshi til".indexOf("yaxshi"));
+
+// mashq 8.  
+console.log("JavaScript JavaScript".lastIndexOf("JavaScript"));
+
+// mashq 9. 
+let date = "2025-08-13".split("-");
+console.log(`${date[2]}/${date[1]}/${date[0]}`);
+
+// mashq 10. 
+console.log("   Salom   ".trim());
+
+// mashq 11. 
+console.log("Men yaxshi ko‘raman Ferrarini".replace(/ /g, "_"));
+
+// mashq 12. 
+console.log("Frontend".toUpperCase());
+
+// mashq 13. 
+console.log("JAVASCRIPT".toLowerCase());
+
+// mashq 14. 
+console.log("Salom dunyo".split(" ").reverse().join(" "));
+
+// mashq 15. 
+console.log("Salom".split("").reverse().join(""));
